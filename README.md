af922be79fbcb62f14ac079f3f362f2a446d2a74
Hari Khalsa <hari@devdiva.com>
2013-04-04 20:52:36 -0700

    GA Lession 12 updating users controller, views and twitter goodies

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Shouter App
+
+Thurs, April 4.
+* Forms, validations... @done
+* Bringing together our controllers and views
+* Some simple html niceness from twitter bootrap
+* Push to Heroku
+* Adding authentication with devise
+
+Office Hours
+* Gil's on Sunday
+* Hari available on Google Hangouts on Saturday
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,76 @@
 class UsersController < ApplicationController
+  
+  # our index method should return a collection of all users
   def index
+
+    # Gets all the users (default order is by creations)
+    #@users = User.all
+    
+    # Gets all the users ordered by username (defaults to ASC)
+    @users = User.order(:username).all
+    
   end
 
+  # our show method should return 1 user...
+  # in this example we are using the begin/rescue block to handle any nil @user instances
   def show
+    begin 
+      @user = User.find params[:id]
+    rescue
+      redirect_to users_path, :alert => "No user by that id."
+    end
   end
 
+  #
   def edit
+    begin 
+      @user = User.find params[:id]
+    rescue
+      redirect_to users_path, :alert => "No user by that id."
+    end
   end
 
   def update
+    begin
+      @user = User.find_by_id(params[:id])
+      if @user.update_attributes(params[:user])
+        redirect_to @user, notice: "User was saved successfully!"
+      else
+        render :edit
+      end
+
+    rescue
+      render :edit
+    end
   end
 
+  # new - method almost ready (let's add in the other form fields to complete)
   def new
     @user = User.new
   end
 
   def destroy
+    # begin 
+    #   @user = User.find params[:id]
+    #   @user.destroy!
+    #   redirect_to users_path, notice: "User was destroyed."
+    # rescue
+    #   redirect_to users_path
+    # end 
   end
 
+  # create - method ready for our prototype app.
   def create
     @user = User.new(params[:user])
-    @user.save!
+
+    if @user.valid? then
+      if @user.save!
+          redirect_to @user, notice: "User was saved successfully!"
+        else
+          render :new
+        end
+    else
+       render :new
+    end
   end
 end
